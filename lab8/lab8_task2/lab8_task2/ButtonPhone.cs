4476c259992836ec157bc3deafff106b155eb2ba
MuzykaMilena <76119202+MilenaM8@users.noreply.github.com>
2020-12-16 22:33:27 +0200

    Add files via upload

---
+++ lab8/lab8_task2/lab8_task2/ButtonPhone.cs
@@ -0,0 +1,21 @@
+//Muzyka_Milena_PD-22
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab8_task2
+{
+    class ButtonPhone : DiskPhone
+    {
+        
+        public bool Buttons = true;
+
+        public override void Call()
+        {
+            Console.WriteLine("Hi! I can call someone using buttons and radio waves!");
+        }
+
+    }
+}
